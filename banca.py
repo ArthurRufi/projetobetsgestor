9bfe200478287d9032856fdb00a926601e8e6223
ArthurRufi <temparthur87@gmail.com>
2023-03-17 16:12:58 -0300

    Introducao as Funcionalidades do modulo clube.py 17/03/2023

--- banca.py
+++ banca.py
@@ -1,4 +1,4 @@
-class banca():
+class Banca():
     def __init__(self) -> None:
         pass
     
--- clubes.py
+++ clubes.py
@@ -1,8 +1,15 @@
-class clube:
-    def __init__(self) -> None:
-        pass
+class Clube:
+    def __init__(self, nome, vitorias, derrotas, desfalques, adversarios):
 
-    def vitorias():
+        self._nome = nome
+        self._vitorias = vitorias
+        self._derrotas = derrotas
+        self._desfalques = desfalques
+        self._adversarios = adversarios
+
+
+    def vitorias(self, vitorias, adversarios):
+
         pass
 
 
